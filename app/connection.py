eb89558651c9c5b7b257cf3d597a6471ce6bfd1b
Rene A. Zelaya <renezelaya@Renes-MacBook-Pro.local>
2014-11-22 10:11:04 -0500

    Adding some extra attributes to models.

--- app/connection.py
+++ app/connection.py
@@ -1,4 +1,7 @@
+
 class Connection:
     def __init__(self, regions, definition):
         self.from = definition[0]
         self.to = definition[1]
+        self.weight = definition[2]
+
--- app/model.py
+++ app/model.py
@@ -1,17 +1,33 @@
+import sys
 
-class Model:
-    def __init__(self):
-        pass
+class Node:
 
-    def load(self, filename):
+    def __init__(self, name, json_file):
+        self.name = name
+        self.population = 0
+        self.load_json_file(json_file)
+        self.state = "Susceptible"
+        
+
+    def change_state(self, state):
+        self.state = state
+
+    def set_population(self, population):
+        self.population = population
+
+    def load_json_file(self, filename):
         import json
-        data = json.loads(open(filename).read())
+        try:
+            data = json.loads(open(filename).read())
 
-        for region in data['regions']:
-            self.regions.append(Region(region))
+            for region in data['regions']:
+                self.regions.append(Region(region))
 
-        for connection in data['connection']:
-            self.connections.append(Connection(self.regions, connection))
+            for connection in data['connection']:
+                self.connections.append(Connection(self.regions, connection))
+
+        except IOError:
+            print("Error: can't find the json file or read the data")
 
 
 
